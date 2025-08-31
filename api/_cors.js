67fd571502c8e67d8ff71de8de4e07dd8a4f8f57
mikeintveld <mw.intveld@gmail.com>
2025-08-31 12:59:52 -0300

    Create _cors.js

---
+++ api/_cors.js
@@ -0,0 +1,14 @@
+export function setCORS(res, origin = process.env.ALLOWED_ORIGIN || '*') {
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Vary', 'Origin');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+}
+
+export function handlePreflight(req, res) {
+  if (req.method === 'OPTIONS') {
+    setCORS(res);
+    return res.status(204).end();
+  }
+  return false; // not a preflight
+}
